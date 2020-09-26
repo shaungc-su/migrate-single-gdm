94cf35ebc8825f6cca18cf6008373fda8d938000
Shaung Cheng <shaungc@SC-C02XQ5Q7JG5J.attlocal.net>
2020-09-25 23:19:08 -0700

    fix minor error

--- install-gci-vci-serverless-code.sh
+++ install-gci-vci-serverless-code.sh
@@ -1,16 +1,17 @@
 # please run this script at the same directory from terminal
 
+# if any error occur, abort immediately
+set -e
+
 eval $(egrep -v '^#' .env | xargs)
 
-if [ -z "${GCI_VCI_SERVERLESS_RELATIVE_PATH}"]; then
+if [[ -z "${GCI_VCI_SERVERLESS_RELATIVE_PATH}" ]]
+then
     echo "Environment variant GCI_VCI_SERVERLESS_RELATIVE_PATH is required. Please specofy it in .env"
     return
 else
     echo "GCI_VCI_SERVERLESS_RELATIVE_PATH is ${GCI_VCI_SERVERLESS_RELATIVE_PATH}"
 fi
-
-# if any error occur, abort immediately
-set -e
 
 cp setup.py ${GCI_VCI_SERVERLESS_RELATIVE_PATH}/
 
