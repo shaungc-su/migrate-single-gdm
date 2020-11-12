09496f661e7f9a7101e4639f132f76790ef55537
Shaung Cheng <shaungc@SC-C02XQ5Q7JG5J.attlocal.net>
2020-11-12 09:34:17 -0800

    add comment to help config

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 setup(
+    # refer to python doc for config:
+    # https://docs.python.org/3/distutils/setupscript.html#distutils-additional-files
     name='gcivcisls', version='1.3', packages=['src', 'src.models', 'src.utils'],
     package_data={'src.models': ['*.json']},
 )
